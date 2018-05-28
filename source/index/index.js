fd5177dd85485ca1aeed76108cae6a9fbd9ab276
Mariya Molchanova <molchanova.m2903@gmail.com>
2018-05-28 23:56:18 +0300

    recived a list of friends, finished styling

--- source/index/index.js
+++ source/index/index.js
@@ -1,50 +1,7 @@
-// import './base.scss';
-// import './app.scss';
+import 'normalize.css';
+import '../base.scss';
+import './index.scss';
 
-VK.init({
-    apiId: 6491719
-});
+import vk from '../vk.js';
 
-function auth() {
-    return new Promise((resolve, reject) => {
-        VK.Auth.login(data => {
-            if (data.session) {
-                resolve();
-            } else {
-                reject(new Error('Не удалось авторизоваться'));
-            }
-        }, 2)
-    });
-}
-
-function callAPI(method, params) {
-    params.v = '5.76';
-
-    return new Promise((resolve, reject) => {
-        VK.api(method, params, (data) => {
-            if (data.error) {
-                reject(data.error);
-            } else {
-                resolve(data.response);
-            }
-        });
-    })
-}
-
-(async () => {
-    try {
-        await auth();
-        const friends = await callAPI('friends.get', { fields: 'photo_100' });
-        const template = document.querySelector('#user-template').textContent;
-        const render = Handlebars.compile(template);
-        const html = render(friends);
-        const results = document.querySelector('#results');
-
-        results.innerHTML = html;
-    } catch (e) {
-        console.error(e);
-    }
-})();
-
-
-//    friends.get 
+vk();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const PATHS = {
     source: path.join(__dirname, 'source'),
@@ -15,8 +20,56 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: PATHS.source + '/index/index.html'
+            template: PATHS.source + '/index/index.html',
+            // chunks: ['index', 'common']
         }),
-        new CleanWebpackPlugin('build')
-    ]
+        new CleanWebpackPlugin('build'),
+        new ExtractTextPlugin('[name].css'),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'common'
+        // }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessorOptions: { discardComments: {removeAll: true } }
+        }),
+        new StyleLintPlugin({
+            configFile: './.stylelintrc'
+        }),
+        new UglifyJSPlugin({
+            sourceMap: true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    publicPath: './',
+                    use: ['css-loader','sass-loader'],
+                })
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })                
+            },
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "eslint-loader",
+                options: {
+                    fix: true
+                }            
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[ext]'
+                }
+            } 
+        ]
+    }
 };
